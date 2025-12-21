9b3f9c58cabd83466746f37c6f94e5374356afbd
corantin antony <corantin.antony@gmail.com>
2025-12-21 13:13:45 +0100

    end create squelette

--- frontend/src/views/registration.vue
+++ frontend/src/views/registration.vue
@@ -15,20 +15,26 @@ const route = useRoute()
       <span class="text-xl font-semibold">Retour</span>
     </button>
 
-    <div class="bg-white rounded-2xl shadow-2xl p-6 pt-2 size-auto w-fit">
-      <p class="text-gray-800 font-medium flex justify-center">Inscription</p>
-      <p class="text-gray-800 flex justify-start">Email :</p>
-        <input type="text" name="email" class=" bg-gray-300 rounded-md p-1 w-11/12 mb-4" />
-      <p class="text-gray-800 flex justify-start">Nom d'utilisateur :</p>
-        <input type="text" name="username" class=" bg-gray-300 rounded-md p-1 w-11/12 mb-4" />
-      <p class="text-gray-800 flex justify-start">Mot de passe :</p>
-        <input type="password" name="password" class=" bg-gray-300 rounded-md p-1 w-11/12 mb-4" />
-      <p class="text-gray-800 flex justify-start">Confirmer le mot de passe :</p>
-        <input type="password" name="confirm_password" class=" bg-gray-300 rounded-md p-1 w-11/12 mb-4" />
-        <div class="flex justify-center mt-4">
-          <button class="bg-[#64067C] text-white rounded-md px-4 py-2 hover:bg-[#850451] transition-colors">S'inscrire</button>
-        </div>
+    <div class="bg-white rounded-2xl shadow-2xl p-6 pt-2 w-80"> <p class="text-gray-800 font-bold flex justify-center mb-4">Inscription</p>
+  
+    <p class="text-gray-800 font-bold flex justify-start">Email :</p>
+    <input type="text" name="email" class="bg-gray-300 rounded-md p-1 mb-4 w-full" />
+    
+    <p class="text-gray-800 font-bold flex justify-start">Nom d'utilisateur :</p>
+    <input type="text" name="username" class="bg-gray-300 rounded-md p-1 w-full mb-4" />
+    
+    <p class="text-gray-800 font-bold flex justify-start">Mot de passe :</p>
+    <input type="password" name="password" class="bg-gray-300 rounded-md p-1 w-full mb-4" />
+    
+    <p class="text-gray-800 font-bold flex justify-start">Confirmer le mot de passe :</p>
+    <input type="password" name="confirm_password" class="bg-gray-300 rounded-md p-1 w-full mb-4" />
+    
+    <div class="flex justify-center mt-4">
+      <button class="bg-[#64067C] text-white rounded-md px-4 py-2 hover:bg-[#850451] transition-colors" name="submit">
+        S'inscrire
+      </button>
     </div>
+  </div>
     
 
   </div>
