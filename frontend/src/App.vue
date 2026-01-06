d72bf73a535ca2a0d623289c6860bda828f78f89
Lucasvlt <lvalentes29@gmail.com>
2026-01-06 08:25:45 +0100

    Correction demandé

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import "./assets/main.css";
 import VibzHeader from "./components/VibzHeader.vue";
 import VibzFooter from "./components/VibzFooter.vue";
 </script>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import accueil from "@/views/accueil.vue";
+import accueil from "@/views/Accueil.vue";
 
 const router = createRouter({
   history: createWebHistory(),
